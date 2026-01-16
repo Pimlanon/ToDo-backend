a375029aa3ec37f8368dc674b8dacd7c2cd841e2
nonlaphan <nonlaphan.pim@devdeva.tech>
2026-01-16 18:20:13 +0700

    feat: setup route of User

---
+++ routes/user_routes.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, request, jsonify
+from services.user_service import UserService
+from schemas.user_schema import UserCreate
+
+user_bp = Blueprint("users", __name__)
+service = UserService()
+
+@user_bp.post("")
+def create_user():
+    payload = UserCreate.model_validate(request.json)
+    user = service.create_user(payload)
+    return jsonify(user.to_dict()), 201
+
+@user_bp.get("")
+def get_users():
+    users = service.get_users()
+    return jsonify([u.to_dict() for u in users])
+
+@user_bp.get("/<string:id>")
+def get_user(id):
+    user = service.get_user(id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    return jsonify(user.to_dict())
+
+@user_bp.delete("/<id>")
+def delete_user(id):
+    user = service.delete_user(id)
+
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    return jsonify({"message": "User deleted successfully"}), 200
