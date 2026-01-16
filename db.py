c1d191a9f10c51d0ac99b1a9c1ee8b6e28f26220
nonlaphan <nonlaphan.pim@devdeva.tech>
2026-01-16 18:21:43 +0700

    refac: clear print and py file

--- db.py
+++ db.py
@@ -4,9 +4,6 @@ import libsql_client
 
 load_dotenv()
 
-print("URL:", os.getenv("TURSO_DATABASE_URL"))
-print("TOKEN:", os.getenv("TURSO_AUTH_TOKEN")[:10], "...")
-
 DATABASE_URL = os.getenv("TURSO_DATABASE_URL").replace("libsql://", "https://")
 AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
 
@@ -14,3 +11,6 @@ client = libsql_client.create_client_sync(
     url=DATABASE_URL,
     auth_token=AUTH_TOKEN,
 )
+
+def get_db():
+    return client
