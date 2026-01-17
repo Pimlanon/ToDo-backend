3912807820813c627d570f7af3ca695f6c34bf0c
nonlaphan <nonlaphan.pim@gmail.com>
2026-01-17 11:06:15 +0700

    feat: add basic repo of Tasks

---
+++ repositories/task_repo.py
@@ -0,0 +1,38 @@
+from db import get_db
+from models.task_model import Task
+
+class TaskRepository: 
+
+    def create(self, task: Task):
+        db = get_db()
+        response = db.execute("""
+            INSERT INTO tasks (id, page_id, title, status, position, created_at, description, priority, due_date)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, [task.id, task.page_id, task.title, task.status, task.position, task.created_at, task.description, task.priority, task.due_date])
+        print(f'response: {response}')
+        return 'task'
+
+    def find_all(self):
+        db = get_db()
+        result = db.execute("SELECT * FROM tasks")
+        columns = result.columns
+        return [
+            Task(**dict(zip(columns, row)))
+            for row in result.rows
+        ]
+    
+    def find_by_id(self, task_id: str):
+        db = get_db()
+        result = db.execute("SELECT * FROM tasks WHERE id = ?", [task_id])
+        if not result.rows:
+            return None
+
+        row = result.rows[0]
+        columns = result.columns
+        data = dict(zip(columns, row))
+
+        return Task(**data)
+
+    def delete(self, task_id: str):
+        db = get_db()
+        db.execute("DELETE FROM tasks WHERE id = ?", [task_id])
