4a937c0307f6b951c37f05451d495e0f295a33bf
nonlaphan <nonlaphan.pim@gmail.com>
2026-01-17 14:17:53 +0700

    feat: add create default page after register

--- services/user_service.py
+++ services/user_service.py
@@ -2,30 +2,40 @@ from repositories.user_repo import UserRepository
 from models.user_model import User
 from datetime import datetime
 from schemas.user_schema import UserCreate
+from services.page_service import PageService
 from utilities.password import hash_password
+from errors import ConflictError, InternalError
 import uuid
 
 repo = UserRepository()
+page_service = PageService()
 
 class UserService:
 
     def create_user(self, data: UserCreate):
         # check duplicate email
         if repo.email_exists(data.email):
-            raise ValueError("Email already exists")
+            raise ConflictError("Email already exists")
         
-        # hash password
-        hashed_password = hash_password(data.password)
-
         user = User(
             id=str(uuid.uuid4()),
             username=data.username,
-            password=hashed_password,
+            password=hash_password(data.password), # hash password
             email=data.email,
             created_at=datetime.utcnow().isoformat()
         )
+
+        # create user
         repo.create(user)
-        return user
+
+        try:
+            # create default page for new user
+            page_service.create_page(user.id)
+
+            return user
+        
+        except Exception :
+            raise InternalError("User created but default page creation failed")
 
     def get_users(self):
         return repo.find_all()
